a2e0279ca5c8cd88838052ba2aefae5b1bac5b04
omar cruz <cruzherreria1@gmail.com>
2023-03-27 23:03:18 -0300

    modelChangeCompany

--- models/company.js
+++ models/company.js
@@ -6,24 +6,22 @@ const { Schema } = mongoose
 const companySchema = new Schema({
   nameCompany: { type: String, require: true },
   identifier:{type:Number,require:true},
-  phone: { type: Number, require: true },
+  phone: { type: String, require: true },
   address: { type: String, require: true },
   notesComp: { type: String, require: true },
+  email:{type:String, require:false},
   avatar: { type: String, required: false },
   Category: {
     type: Schema.Types.ObjectId,
     ref: 'Category'
   },
-  // president: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'Client',
-  //   nameClient: String
-  // },
   country:{type:String,require:true},
   cityName:{type:String,require:true},
-  level:{type:Number,require:false},  
+  level:{type:Number,require:false},
+  levelPay:{type:Boolean,require:false},//0=false = empresa que NO pago 
   status:{type:Boolean,default:true},
-  siteWeb:{type:String,require:false}
+  siteWeb:{type:String,require:false},
+  branchOffice:[{type:String,require:false}]
 })
 
 module.exports = mongoose.model('Company', companySchema)
